8784788254f1562b24e258fcd476bd510b5c81e3
Adel LATIBI <alatibi@campus-eni.fr>
2022-03-16 13:12:35 +0100

    add bootstrap

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,20 +13,19 @@ class MainController extends AbstractController
     /**
      * @Route("/main", name="app_main")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-
-        $em = $this->getDoctrine()->getManager();
-        $book = new Book();
-        $book->setTitle("The wolf of wall street")
-             ->setAuthor("YYYY")
-             ->setPages("231")
-             ->setDatePublished(new \DateTime('NOW')); 
-             
-        $em->persist($book);
-        $em->flush();
-
-
-        dd("salut");
+        // obtenir Manager interface
+        // $em = $this->getDoctrine()->getManager();
+        // creation de l'objet 
+        // $book = new Book();
+        // $book->setTitle("The wolf of wall street")
+        //      ->setAuthor("YYYY")
+        //      ->setPages("231")
+        //      ->setDatePublished(new \DateTime('NOW')); 
+        // ajout dans bdd     
+        // $em->persist($book);
+        // $em->flush();
+        return $this->render("main/index.html.twig");
     }
 }
